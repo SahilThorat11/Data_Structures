ca139a5510ecc81a6c38afdeb6cdec589acab4ea
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-19 11:44:41 +0530

    DoublyLL with InsertFirst

---
+++ C_Programming/Linked_List/Doubly_Linear_Linked_List/program01.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node * next;
+    struct node * prev;       // $
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+    newn -> prev = NULL;      // $
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        (*first) -> prev = newn;  // $
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+    newn -> prev = NULL;      // $
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        
+    }
+}
+
+void InsertAtPos(PPNODE first, int no, int iPos)
+{}
+
+void DeleteFirst(PPNODE first)
+{}
+
+void DeleteLast(PPNODE first)
+{}
+
+void DeleteAtPos(PPNODE first, int iPos)
+{}
+
+void Display(PNODE first)
+{}
+
+int Count(PNODE first)
+{
+    return 0;
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    return 0;
+}
+
+
+
+/*
+
+$ : This line is new compare to SinglyLL.
+    Remaining all code is same
+
+*/
