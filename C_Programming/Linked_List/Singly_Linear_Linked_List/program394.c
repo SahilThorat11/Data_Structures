cbf628ac3d188d6e8d33608c5da1b85dde1886e5
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-19 11:41:42 +0530

    SinglyLL with all Functions

---
+++ C_Programming/Linked_List/Singly_Linear_Linked_List/program394.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node * next;
+    struct node * prev;       // $
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{}
+
+void InsertLast(PPNODE first, int no)
+{}
+
+void InsertAtPos(PPNODE first, int no, int iPos)
+{}
+
+void DeleteFirst(PPNODE first)
+{}
+
+void DeleteLast(PPNODE first)
+{}
+
+void DeleteAtPos(PPNODE first, int iPos)
+{}
+
+void Display(PNODE first)
+{}
+
+int Count(PNODE first)
+{
+    return 0;
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    return 0;
+}
+
+
+
+/*
+
+$ : This line is new compare to SinglyLL.Remaining all code is same
+
+*/
