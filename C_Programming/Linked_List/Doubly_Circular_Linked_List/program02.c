629cb6eb6f58e80f0652daf289e649780a8ea388
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-23 11:19:47 +0530

    InsertFirst Function

---
+++ C_Programming/Linked_List/Doubly_Circular_Linked_List/program02.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+    struct node * prev;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, PPNODE last, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+    newn -> prev = NULL;
+
+    if((*first == NULL) && (*last == NULL))
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        (*first) -> prev = newn;
+        *first = newn;
+    }
+
+    (*last) -> next = *first;
+    (*first) -> prev = *last;
+}
+
+void InsertLast(PPNODE first, PPNODE last, int no)
+{}
+
+void DeleteFirst(PPNODE first, PPNODE last)
+{}
+
+void DeleteLast(PPNODE first, PPNODE last)
+{}
+
+void Display(PNODE first, PNODE last)
+{}
+
+int Count(PNODE first, PNODE last)
+{
+    return 0;
+}
+
+void InsertAtPos(PPNODE first, PPNODE last, int no, int Pos)
+{}
+
+void DeleteAtPos(PPNODE first, PPNODE last, int Pos)
+{}
+
+int main()
+{
+    PNODE head = NULL;
+    PNODE tail = NULL;
+
+    int iRet = 0;
+
+    InsertFirst(&head, &tail, 51);
+    InsertFirst(&head, &tail, 21);
+    InsertFirst(&head, &tail, 11);
+
+    return 0;
+}
