7ca28688b1ace368696a0986a1f46c77a73384d4
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-24 12:41:23 +0530

    InsertAtPosition Function

---
+++ Cpp_Programming/Linked_List/Singly_Linear_Linked_List/program11.cpp
@@ -0,0 +1,232 @@
+// Use of this->
+
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+
+class SinglyLL
+{
+    public :
+        PNODE first;
+        int iCount;
+
+        SinglyLL()
+        {
+            this -> first = NULL;
+            this -> iCount = 0;
+        }
+
+        void InsertFirst(int no)   // Updated
+        {
+            PNODE newn = NULL;
+
+            newn = new NODE;
+
+            newn -> data = no;
+            newn -> next = NULL;
+
+            newn -> next = this -> first;
+            this -> first = newn;
+    
+            this -> iCount++;
+        }
+
+        void InsertLast(int no)
+        {
+            PNODE newn = NULL;
+            PNODE temp = NULL;
+
+            newn = new NODE;
+
+            newn -> data = no;
+            newn -> next = NULL;
+
+            if(this -> iCount == 0)               // Updated
+            {
+                this -> first = newn;
+            }
+            else
+            {
+                temp = this -> first;
+
+                while(temp -> next != NULL)
+                {
+                    temp = temp -> next;
+                }
+
+                temp -> next = newn;
+            }
+
+            this -> iCount++;
+        }
+
+        void DeleteFirst()
+        {
+            PNODE temp = NULL;
+
+            if(this -> first == NULL)
+            {
+                return;
+            }
+            else if(this -> first -> next == NULL)        // else if(this -> iCount == 1)
+            {
+                delete this -> first;
+                this -> first = NULL;
+            }
+            else
+            {
+                temp = this -> first;
+
+                this -> first = this -> first -> next;
+                delete temp;
+            }
+
+            this -> iCount--;
+        }
+
+        void DeleteLast()
+        {
+            PNODE temp = NULL;
+
+            if(this -> first == NULL)
+            {
+                return;
+            }
+            else if(this -> first -> next == NULL)        // else if(this -> iCount == 1)
+            {
+                delete this -> first;
+                this -> first = NULL;
+            }
+            else
+            {
+                temp = this -> first;
+
+                while(temp -> next -> next != NULL)
+                {
+                    temp = temp -> next;
+                }
+
+                delete temp -> next;
+                temp -> next = NULL;
+            }
+
+            this -> iCount--;
+        }
+
+        void Display()                 // Updated
+        {
+            PNODE temp = NULL;
+            int iCnt = 0;
+
+            temp = this -> first;
+
+            cout << "\n";
+
+            for(iCnt = 1; iCnt <= this -> iCount; iCnt++)
+            {
+                cout << "| " << temp->data << " | -> ";
+                temp = temp->next;
+            }
+
+            cout << "NULL\n\n";
+        }
+
+        int Count()
+        {
+            return this -> iCount;
+        }
+
+        void InsertAtPos(int no, int Pos)
+        {
+            int iCnt = 0;
+            PNODE newn = NULL;
+            PNODE temp = NULL;
+
+            if (Pos < 1 || Pos > this -> iCount + 1)
+            {
+                cout << "Invalid position\n";
+                return;
+            }
+
+            if(Pos == 1)
+            {
+                this -> InsertFirst(no);
+            }
+            else if(Pos == this -> iCount + 1)
+            {
+                this -> InsertLast(no);
+            }
+            else
+            {
+                newn = new NODE;
+
+                newn -> data = no;
+                newn -> next = NULL;
+
+                temp = this -> first;
+
+                for(iCnt = 1; iCnt < Pos - 1; iCnt++)
+                {
+                    temp = temp -> next;
+                }
+
+                newn -> next = temp -> next;
+                temp -> next = newn;
+
+                this -> iCount++;
+            }
+        }
+
+        void DeleteAtPos(int Pos)
+        {}
+};
+
+int main()
+{
+    SinglyLL obj;
+    int iRet = 0;
+
+    obj.InsertFirst(51);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+
+    obj.Display();
+    iRet = obj.Count();
+    cout<<"Number of nodes are : "<<iRet<<"\n";
+
+    obj.InsertLast(101);
+    obj.InsertLast(111);
+    obj.InsertLast(121);
+
+    obj.Display();
+    iRet = obj.Count();
+    cout<<"Number of nodes are : "<<iRet<<"\n";
+
+    obj.InsertAtPos(75, 4);
+
+    obj.Display();
+    iRet = obj.Count();
+    cout<<"Number of nodes are : "<<iRet<<"\n";
+
+    obj.DeleteFirst();
+
+    obj.Display();
+    iRet = obj.Count();
+    cout<<"Number of nodes are : "<<iRet<<"\n";
+
+    obj.DeleteLast();
+
+    obj.Display();
+    iRet = obj.Count();
+    cout<<"Number of nodes are : "<<iRet<<"\n";
+
+    return 0;
+}
