3526770adf3724b4826d188524ec5daf68d8f4c3
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-01 12:12:18 +0530

    Number is present or not in linked list

---
+++ C_Programming/Assignments/Assignment_44/Program44_1.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+bool Search(PNODE first, int iNO)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        if(temp -> data == iNO)
+        {
+            return true;
+        }
+
+        temp = temp -> next;
+    }
+
+    return false;
+}
+
+int main()
+{
+    int iRet = 0;
+    bool bRet = false;
+    PNODE head = NULL;
+
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    bRet = Search(head, 11);
+
+    if(bRet == true)
+    {
+        printf("Number is present in linked list.\n");
+    }
+    else
+    {
+        printf("Number is not present linked list.\n");
+    }
+
+    return 0;
+}
