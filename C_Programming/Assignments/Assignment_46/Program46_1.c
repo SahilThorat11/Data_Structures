ba2372afdf26ffd33c5ae6b706efa529c74d596a
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-01 16:11:36 +0530

    Display Element less than X

---
+++ C_Programming/Assignments/Assignment_46/Program46_1.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+void DisplayGreater(PNODE first, int iNo)
+{
+    PNODE temp = first;
+
+    while(temp != NULL)
+    {
+        if(temp->data > iNo)
+        {
+            printf("| %d | -> ", temp -> data);
+        }
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int iRet = 0, iValue = 0;
+    PNODE head = NULL;
+
+    InsertFirst(&head, 75);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n\n", iRet);
+
+    printf("Enter the number : ");
+    scanf("%d", &iValue);
+
+    printf("Elements Greater than %d are : \n", iValue);
+    DisplayGreater(head, iValue);
+
+    return 0;
+}
