4f515534b78b21979353ba021334170dff6ad593
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-01 13:11:32 +0530

    Display Even numbers from the linked list

---
+++ C_Programming/Assignments/Assignment_44/Program44_5.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+void DisplayEven(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("Even elements from the linked list are : \n");
+
+    while(temp != NULL)
+    {
+        if((temp -> data % 2) == 0)
+        {
+            printf("| %d | -> ", temp -> data);
+        }
+
+        temp = temp -> next;
+    }
+}
+
+int main()
+{
+    int iRet = 0, iValue;
+    bool bRet = false;
+    PNODE head = NULL;
+
+    InsertFirst(&head, 112);
+    InsertFirst(&head, 74);
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n\n", iRet);
+
+    DisplayEven(head);
+
+    return 0;
+}
