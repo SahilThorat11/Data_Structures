b4fa8a5766b23e1fb681b01823148ddb6bf94817
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-03 15:56:15 +0530

    Function to Diplay Linked list in reverse order

---
+++ C_Programming/Assignments/Assignment_47/Program47_1.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+void DisplayReverse(PNODE first)
+{
+    if(first == NULL)
+    {
+        return;
+    }
+
+    DisplayReverse(first->next);
+    printf("| %d | -> ", first->data);
+}
+
+int main()
+{
+    int iRet = 0, iValue = 0;
+    PNODE head = NULL;
+
+    InsertFirst(&head, 75);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n\n", iRet);
+
+    printf("Elements in Reverse order are followes : \n");
+    DisplayReverse(head);
+
+    return 0;
+}
