db7c6547c245c86e79f12c0ec49a31e0927e6834
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-03 16:01:00 +0530

    Replace Even numbers from the linked list with 0

---
+++ C_Programming/Assignments/Assignment_48/Program48_1.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+void ReplaceEven(PNODE first)
+{
+    PNODE temp = NULL;
+    int iNo = 0;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iNo = temp -> data;
+
+        if((iNo % 2) == 0)
+        {
+            iNo = 0;
+        }
+
+        printf("| %d | -> ", iNo);
+        temp = temp -> next;
+    }
+}
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+
+    InsertFirst(&head, 121);
+    InsertFirst(&head, 112);
+    InsertFirst(&head, 54);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n\n", iRet);
+
+    printf("Linked list after Replace Even numbers with 0 : \n");
+    ReplaceEven(head);
+
+    return 0;
+}
