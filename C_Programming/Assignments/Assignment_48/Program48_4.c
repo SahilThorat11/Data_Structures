f1acfdebefe70f0ddeccf53d03a8c29c9c70efca
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-03 16:03:38 +0530

    Function to Display Elements at Even position

---
+++ C_Programming/Assignments/Assignment_48/Program48_4.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNO)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNO;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+void DisplayEvenPosition(PNODE first)
+{
+    PNODE temp = NULL;
+    int iPos = 0;
+
+    temp = first;
+    iPos = 1;
+
+    while(temp != NULL)
+    {
+        if((iPos % 2) == 0)
+        {
+            printf("| %d | -> ", temp->data);
+        }
+
+        temp = temp->next;
+        iPos++;
+    }
+
+    printf("NULL\n");
+}
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+
+    InsertFirst(&head, 151);
+    InsertFirst(&head, 121);
+    InsertFirst(&head, 111);
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n\n", iRet);
+
+    printf("Elements of Even positions are : \n");
+    DisplayEvenPosition(head);
+
+    return 0;
+}
