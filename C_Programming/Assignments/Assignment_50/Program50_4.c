7b2fdf0b4b1625263dde2d962332744e5820f76d
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-05 13:07:12 +0530

    Display Alternate Nodes

---
+++ C_Programming/Assignments/Assignment_50/Program50_4.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = iNo;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp -> data);
+        temp = temp -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+    PNODE temp = NULL;
+
+    temp = first;
+
+    while(temp != NULL)
+    {
+        iCount++;
+        temp = temp -> next;
+    }
+    
+    return iCount;
+}
+
+void DisplayAlternate(PNODE first)
+{
+    PNODE temp = NULL;
+
+    temp = first;
+
+    printf("Alternate nodes are:\n");
+
+    while(temp != NULL)
+    {
+        printf("| %d | -> ", temp->data);
+
+        if(temp->next != NULL)
+        {
+            temp = temp->next->next;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    printf("NULL\n");
+}
+
+
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+
+    InsertFirst(&head, 151);
+    InsertFirst(&head, 121);
+    InsertFirst(&head, 111);
+    InsertFirst(&head, 101);
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n\n", iRet);
+
+    DisplayAlternate(head);
+
+    return 0;
+}
