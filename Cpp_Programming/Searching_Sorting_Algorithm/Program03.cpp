82825baaee64a53db2f60ad273a90be986a49751
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-14 21:00:19 +0530

    BiDirectionalsearch function

---
+++ Cpp_Programming/Searching_Sorting_Algorithm/Program03.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        ArrayX(int no)
+        {
+            cout<<"Inside constructor\n";
+            iSize = no;
+            Arr = new int[iSize];
+        }
+
+        ~ArrayX()
+        {
+            cout<<"Inside destructor\n";
+            delete [] Arr;
+        }
+
+        void Accept()
+        {
+            int iCnt = 0;
+
+            cout<<"Enter the elements : \n";
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                cin>>Arr[iCnt];
+            }
+        }
+
+        void Display()
+        {
+            int iCnt = 0;
+
+            cout<<"Elemets of the array are : \n";
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                cout<<Arr[iCnt]<<"\t";
+            }
+
+            cout<<"\n";
+        }
+
+        bool LinearSearch(int iNo)
+        {
+            bool bFlag = false;
+            int iCnt = 0;
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                if(Arr[iCnt] == iNo)
+                {
+                    bFlag = true;
+                    break;
+                }
+            }
+            
+            return bFlag;
+        }
+
+        bool BiDirectionalSearch(int iNo)
+        {
+            bool bFlag = false;
+            int iStart = 0;
+            int iEnd  = 0;
+
+            for(iStart = 0, iEnd = iSize - 1; iStart <= iEnd; iStart++, iEnd--)
+            {
+                if((Arr[iStart] == iNo) || (Arr[iEnd] == iNo))
+                {
+                    bFlag = true;
+                    break;
+                }
+            }
+            
+            return bFlag;
+        }
+};
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter the number of elements : \n";
+    cin>>iValue;
+
+    ArrayX aobj(iValue);
+
+    aobj.Accept();
+    aobj.Display();
+
+    if(aobj.LinearSearch(21))
+    {
+        cout << "Element is present.\n";
+    }
+    else
+    {
+        cout << "Element is Not present.\n";
+    }
+
+    if(aobj.BiDirectionalSearch(21))
+    {
+        cout << "Element is present.\n";
+    }
+    else
+    {
+        cout << "Element is Not present.\n";
+    }
+    
+    return 0;
+}
