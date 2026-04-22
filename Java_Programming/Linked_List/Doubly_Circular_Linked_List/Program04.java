74ec05e012e337e72950a9e923f4114c31a64066
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-22 19:40:05 +0530

    Function to count nodes

---
+++ Java_Programming/Linked_List/Doubly_Circular_Linked_List/Program04.java
@@ -0,0 +1,111 @@
+// Doubly Circular
+class node
+{
+    public int data;
+    public node next;
+    public node prev;
+
+    public node(int no)
+    {
+        this.data = no;
+        this.next = null;
+        this.prev = null;
+    }
+}
+class DoublyCL
+{
+    private node first;
+    private node last;
+    private int iCount;
+
+    public DoublyCL()
+    {
+        System.out.println("Object of DoublyCL gets created.");
+        this.first = null;
+        this.last = null;
+        this.iCount = 0;
+    }
+
+    public void InsertFirst(int no)
+    {
+        node newn = new node(no);
+
+        if(this.first == null && this.last == null)
+        {
+            // First node points to itself in both directions
+            this.first = newn;
+            this.last = newn;
+        }
+        else
+        {
+            // Connect newn between last and first
+            newn.next = this.first;
+            this.first.prev = newn;
+            this.first = newn;
+        }
+
+        // Maintain circular links
+        this.first.prev = this.last;
+        this.last.next = this.first;
+
+        this.iCount++;
+    }
+
+    public void InsertLast(int no)
+    {}
+
+    public void InsertAtPos(int no, int Pos)
+    {}
+
+    public void DeleteFirst()
+    {}
+
+    public void DeleteLast()
+    {}
+
+    public void DeleteAtPos(int Pos)
+    {}
+
+    public void Display()
+    {
+        node temp = null;
+        temp = this.first;
+
+        System.out.println();
+
+        while(temp != this.last)
+        {
+            System.out.print("| " + temp.data + " | <=> ");
+            temp = temp.next;
+        }
+
+        // Print last node
+        System.out.print("| " + temp.data + " | <=> ");
+        System.out.println();
+    }
+
+    public int Count()
+    {
+        return this.iCount;
+    }
+}
+class Program04
+{
+    public static void main(String A[])
+    {
+        DoublyCL obj = null;
+        int iRet = 0;
+        obj = new DoublyCL();
+
+        obj.InsertFirst(51);
+        obj.InsertFirst(21);
+        obj.InsertFirst(11);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of nodes are : " + iRet);
+        
+        obj = null;
+        System.gc();
+    }
+}
