72ca55af9786c3f53190717014bb2d09bec5c2cc
SAHIIL <sahilrajaramthorat11@gmail.com>
2026-04-22 17:09:39 +0530

    Final code of DoublyLL

--- Java_Programming/Linked_List/Doubly_Linear_Linked_List/Program10.java
+++ Java_Programming/Linked_List/Doubly_Linear_Linked_List/Program10.java
@@ -1,11 +1,9 @@
 // Doubly Linear 
-
 class node
 {
     public int data;
     public node next;
     public node prev;
-
     public node(int no)
     {
         this.data = no;
@@ -13,49 +11,211 @@ class node
         this.prev = null;
     }
 }
-
 class DoublyLL
 {
     private node first;
-
     private int iCount;
-
     public DoublyLL()
     {
-        System.out.println("Object of DoublyLL gets created.");
         this.first = null;
-
         this.iCount = 0;
     }
 
     public void InsertFirst(int no)
-    {}
+    {
+        node newn = new node(no);
+
+        newn.next = this.first;
+
+        // If list is not empty, set existing first's prev to new node
+        if(this.first != null)
+        {
+            this.first.prev = newn;
+        }
+
+        this.first = newn;
+        this.iCount++;
+    }
 
     public void InsertLast(int no)
-    {}
+    {
+        node newn = new node(no);
+        node temp = null;
+
+        if(this.first == null)
+        {
+            this.first = newn;
+        }
+        else
+        {
+            temp = this.first;
+
+            // Traverse to the last node
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = newn;
+            newn.prev = temp;   // Link back to previous node
+        }
+
+        this.iCount++;
+    }
 
     public void InsertAtPos(int no, int Pos)
-    {}
+    {
+        node newn = null;
+        node temp = null;
+        int iCnt = 0;
+
+        if((Pos < 1) || (Pos > this.iCount + 1))
+        {
+            System.out.println("Invalid Position");
+            return;
+        }
+
+        if(Pos == 1)
+        {
+            this.InsertFirst(no);
+        }
+        else if(Pos == this.iCount + 1)
+        {
+            this.InsertLast(no);
+        }
+        else
+        {
+            newn = new node(no);
+            temp = this.first;
+
+            for(iCnt = 1; iCnt < Pos - 1; iCnt++)
+            {
+                temp = temp.next;
+            }
+
+            // Connect newn between temp and temp.next
+            newn.next = temp.next;
+            newn.prev = temp;
+
+            // Update the next node's prev pointer
+            if(temp.next != null)
+            {
+                temp.next.prev = newn;
+            }
+
+            temp.next = newn;
+            this.iCount++;
+        }
+    }
 
     public void DeleteFirst()
-    {}
+    {
+        if(this.first == null)
+        {
+            return;
+        }
+        else if(this.first.next == null)
+        {
+            this.first = null;
+        }
+        else
+        {
+            this.first = this.first.next;
+            this.first.prev = null;     // New first has no previous node
+        }
+
+        System.gc();
+        this.iCount--;
+    }
 
     public void DeleteLast()
-    {}
+    {
+        node temp = null;
+
+        if(this.first == null)
+        {
+            return;
+        }
+        else if(this.first.next == null)
+        {
+            this.first = null;
+        }
+        else
+        {
+            temp = this.first;
+
+            // Traverse to the last node
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.prev.next = null;      // Second-last node now points to null
+        }
+
+        System.gc();
+        this.iCount--;
+    }
 
     public void DeleteAtPos(int Pos)
-    {}
+    {
+        node temp = null;
+        int iCnt = 0;
+
+        if((Pos < 1) || (Pos > this.iCount))
+        {
+            System.out.println("Invalid Position");
+            return;
+        }
+
+        if(Pos == 1)
+        {
+            this.DeleteFirst();
+        }
+        else if(Pos == this.iCount)
+        {
+            this.DeleteLast();
+        }
+        else
+        {
+            temp = this.first;
+
+            for(iCnt = 1; iCnt < Pos; iCnt++)
+            {
+                temp = temp.next;
+            }
+
+            // Bypass the node to be deleted using both prev and next links
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+
+            System.gc();
+            this.iCount--;
+        }
+    }
 
     public void Display()
-    {}
+    {
+        node temp = null;
+        temp = this.first;
+
+        System.out.println();
+
+        while(temp != null)
+        {
+            System.out.print("| "+temp.data+" | <-> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
 
     public int Count()
     {
         return this.iCount;
     }
 }
-
-class program450
+class Program09
 {
     public static void main(String A[])
      {
@@ -81,29 +241,25 @@ class program450
         System.out.println("Number of nodes are : " + iRet);
 
         obj.InsertAtPos(75, 4);
-
         obj.Display();
         iRet = obj.Count();
         System.out.println("Number of nodes are : " + iRet);
 
         obj.DeleteAtPos(4);
-
         obj.Display();
         iRet = obj.Count();
         System.out.println("Number of nodes are : " + iRet);
 
         obj.DeleteFirst();
-
         obj.Display();
         iRet = obj.Count();
         System.out.println("Number of nodes are : " + iRet);
 
         obj.DeleteLast();
-
         obj.Display();
         iRet = obj.Count();
         System.out.println("Number of nodes are : " + iRet);
-
+        
         // Important for memory deallocation
         obj = null;
         System.gc();
