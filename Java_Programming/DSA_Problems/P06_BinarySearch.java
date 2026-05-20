6dc83557676da4b40964dac253aa01de0f3f13fe
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-05-20 09:53:08 +0530

    BinarySearch

---
+++ Java_Programming/DSA_Problems/P06_BinarySearch.java
@@ -0,0 +1,36 @@
+// P06 - Binary Search
+class P06_BinarySearch 
+{
+    static int binarySearch(int[] arr, int target) 
+    {
+        int low = 0, high = arr.length - 1;
+        while(low <= high) 
+        {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+            {
+                return mid;
+            } 
+            else if (arr[mid] < target) 
+            {
+                low = mid + 1;
+            }
+            else 
+            {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) 
+    {
+        int[] arr = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = binarySearch(arr, target);
+        
+        System.out.println("Binary Search: " + target + " found at index " + result);
+    }
+}
