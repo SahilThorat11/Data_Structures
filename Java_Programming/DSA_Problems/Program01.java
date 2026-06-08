42e00efd0ae0f6eff2df6000e155505cc6ed4589
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-06-08 12:59:15 +0530

    File name changed

---
+++ Java_Programming/DSA_Problems/Program01.java
@@ -0,0 +1,29 @@
+// P01 - Bubble Sort
+public class Program01
+{
+    static void bubbleSort(int arr[]) 
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+        {    
+            for (int j = 0; j < n - i - 1; j++)
+            { 
+                if (arr[j] > arr[j + 1]) 
+                {
+                    int tmp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = tmp;
+                }
+            }    
+        }    
+    }
+
+    public static void main(String A[]) 
+    {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.print("Bubble Sort: ");
+        for(int x : arr) 
+        {
+            System.out.print(x + " ");
+        }
+    }
+}
